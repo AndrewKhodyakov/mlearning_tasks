e4bbb40e056f2f673da174e6212ab701138bb481
AndrewKhodyakov <cbibop.xo@gmail.com>
2016-04-03 23:16:24 +0300

    Начало провки tree

--- tree.py
+++ tree.py
@@ -14,8 +14,10 @@ csv_data = '/home/ajetmania/work_repo/mlearning/titanic.csv'
 if __name__ == "__main__": 
 
     data = pd.read_csv(csv_data)
-    workdata = pd.DataFrame(index=DF.Age.index,\
+    workdata = pd.DataFrame(index=data.Age.index,\
              columns=['Age', 'Sex', 'Pclass', 'Fare', 'Survived'])
     for i in data:
         workdata[i] = data[i]
+
+    print(workdata)
 
