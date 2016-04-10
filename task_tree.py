16fcd371dc5ffdfc415e8471a911f916a3ec2d58
AndrewKhodyakov <cbibop.xo@gmail.com>
2016-04-10 22:41:10 +0300

    Готово неправильное решение надо искать ошибку

--- task_tree.py
+++ task_tree.py
@@ -6,23 +6,29 @@
 """
 import os 
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+from sklearn.externals.six  import StringIO
 
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 #===============================================================================
-csv_data = os.getcwd() + '/titanic.csv'
-path_to_plots = os.getcwd() + '/img/workdata.png'
-#===============================================================================
-
-if __name__ == "__main__" : 
+def load_data(csv_data):
+    """
+    Загрузка и подготовка данных
+    """
 
     data = pd.read_csv(csv_data)
     workdata = data.ix[:,['Age', 'Sex', 'Pclass', 'Fare', 'Survived']]
     workdata = workdata[pd.notnull(workdata['Age'])] #удаляем пропуски
     workdata = workdata.replace(['male', 'female'], [0,1]) #избавлямся от строк
+    return workdata
+
+def plot_data(path_to_plots):
+    """
+    Построение графиков в файл
+    """
 
     figure, ((age, sex), (pclass, fare)) =\
          plt.subplots(2, 2,sharex='col', sharey='row')
@@ -46,7 +52,32 @@ if __name__ == "__main__" :
 
     figure.savefig(path_to_plots)
 
+def fit_data(workdata):pass
+
+#===============================================================================
+
+if __name__ == "__main__" : 
+
+    csv_data = os.getcwd() + '/titanic.csv'
+    path_to_plots = os.getcwd() + '/img/workdata.png'
+
+    workdata = load_data(csv_data)
+
+    plot_data(path_to_plots)
+
     #print(workdata)
 
-    #clf = DecisionTreeClassifier(random_state=241)
+    X = [workdata.ix[i, ['Pclass', 'Fare', 'Age', 'Sex']].get_values()\
+         for i in workdata.index]
+
+    clf = tree.DecisionTreeClassifier(random_state=241)
+    clf = clf.fit(X, workdata.Survived)
+
+    with open(os.getcwd() + "/iris.dot", 'w') as f_dot:
+         f_dot = tree.export_graphviz(clf, out_file=f_dot)
+    
+    importances = clf.feature_importances_   
+
+    print(workdata.ix[0, ['Age', 'Sex', 'Pclass', 'Fare']])
+    print(importances) 
 
