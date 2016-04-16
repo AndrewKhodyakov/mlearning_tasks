1fdf76fb6548aac8432d4ddcee91f9f07b9fb086
AndrewKhodyakov <cbibop.xo@gmail.com>
2016-04-17 00:35:49 +0300

    НАписаол загрузку данных

--- neighbors.py
+++ neighbors.py
@@ -2,10 +2,23 @@
 # -*- coding: utf-8 -*-
 #===============================================================================
 """
-    Метод_к_ближайших_соседей
+    Метод_k_ближайших_соседей
 """
-#===============================================================================
+import os 
+import pandas as pd
+from sklearn import tree
+from sklearn.externals.six  import StringIO
 
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+#===============================================================================
+def load_data(csv_data):
+    """
+    Загрузка и подготовка данных:
+    """
+    data = pd.read_csv(csv_data)
+    return data
 #===============================================================================
 
 if __name__ == "__main__": pass
