97565012f25799a1f7b507a168caaca06494b7ea
AndrewKhodyakov <cbibop.xo@gmail.com>
2016-06-04 23:44:12 +0300

    Начал писать метрики

--- metric_knn.py
+++ metric_knn.py
@@ -3,11 +3,46 @@
 #===============================================================================
 """
     Выбор_метрики_методом_ближайшего_соседа
+    Данные Бостон:
+    1. CRIM: per capita crime rate by town
+        (рейтинг приступности на душу населения по городу)
+    2. ZN: proportion of residential land zoned for lots over 25,000 sq.ft. 
+        (доля жилой земли площадью боле 25000 кв футов)
+    3. INDUS: proportion of non-retail business acres per town 
+        (доля не розничного(торгового) бизнеса в городе)
+    4. CHAS: Charles River dummy variable (= 1 if tract bounds river; 0 otherwise) 
+        (фиктивная переменная расположения относительно реки)
+    5. NOX: nitric oxides concentration (parts per 10 million) 
+        (концентрация оксидов азота (частей на 10 миллионов))
+    6. RM: average number of rooms per dwelling 
+        (среднее колличество комнат в жилом помещении)
+    7. AGE: proportion of owner-occupied units built prior to 1940 
+        (колличество елиниц жилья в собственности, построенных до 1940 ого года)
+    8. DIS: weighted distances to five Boston employment centres 
+        (взвешанные расстония до 5 Бостноских центров занятости)
+    9. RAD: index of accessibility to radial highways 
+        (индекс доступности к радиальным трассам)
+    10. TAX: full-value property-tax rate per $10,000 
+        (полноценная ставка налога на недвижимость)
+    11. PTRATIO: pupil-teacher ratio by town 
+        (соотношение учеников и учителей по городу)
+    12. B: 1000(Bk - 0.63)^2 where Bk is the proportion of blacks by town 
+        (где Bk - доля чернокожих по городу)
+    13. LSTAT: % lower status of the population 
+        (процент начеления с низким статусом)
+    14. MEDV: Median value of owner-occupied homes in $1000's
+        (медиана домов в собственности в каждой 1000 долларов)
 """
 import os 
+
 from sklearn import datasets
 from sklearn import preprocessing
+
+from sklearn.cross_validation import KFold
+from sklearn.neighbors import KNeighborsRegressor
+
 import pandas as pd
+import numpy as np
 
 import matplotlib
 matplotlib.use('Agg')
@@ -59,13 +94,25 @@ def data_preparation(all_data, features_colomns, label, noramlization=False):
     """
     Подготовка данных для обрабоки, приведение данных к одному масштабу
     """
-    pass
+    out = namedtuple('PreporatedData', ['features', 'labels'])
+    out.labels = all_data[label].values
+
+    if noramlization:
+        out.features = preprocessing.scale(all_data.ix[:, features_colomns].get_values())
+    else:
+        out.features = all_data.ix[:, features_colomns].get_values()
+
+    return out
+
 
 def do_varibale_p():
     """
     Изменение параметра метрики миньковского
     """
-    pass
+    p_value = np.linspace(1,10,200)
+    kfold = KFold(len(data_set.features), n_folds=5, shuffle=True, random_state=42)
+    
+
 #===============================================================================
 
 if __name__ == "__main__": 
