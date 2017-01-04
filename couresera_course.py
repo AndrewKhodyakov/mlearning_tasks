d1e3bbc0901312a616c27109ffe0b95514fde519
AndrewKhodyakov <cbibop.xo@gmail.com>
2017-01-04 23:39:14 +0300

    поганял тесты, теперь пробую запуск задачи

--- couresera_course.py
+++ couresera_course.py
@@ -5,61 +5,138 @@
 """
 import os
 import sys
-import uuid
 
 sys.path.append(os.getcwd())
-
-import json
-import datetime
 
 import luigi
 import pandas as pd
 
+from sklearn.linear_model import Perceptron
 from sklearn.preprocessing import StandardScaler
 
-from utils import strip_meta_path
-from utils import get_meta_data
-from utils import rewrite_file
+
+path_to_data = lambda filename: os.environ.get('DATA_DIR', '.') + '/' + filename
+path_to_result = lambda filename: os.environ.get('RESULT_DIR', '.') + '/' + filename
 
 class TasksSet(luigi.WrapperTask):
     """
     Вся последовательность задач
     """
     def requires(self):
-        yield SecondWeekLinearMethods(
-            param={'train':, 'test':})
+        """
+        Тут задачи
+        """
+        yield SecondWeekLinearMethods(\
+            param={'train':path_to_data('SecondWeekLinearMethods_train.csv'),\
+                'test':path_to_data('SecondWeekLinearMethods_test.csv'),\
+                'scale':False})
 
-class TrainSimpleClassifierModels(luigi.Task):
-    """
-    Класс для построения простейших моделей классификации
-    """
-    def run(self):
-        """
-        """
-        pass
+        yield SecondWeekLinearMethods(\
+            param={'train':path_to_data('SecondWeekLinearMethods_train.csv'),\
+                'test':path_to_data('SecondWeekLinearMethods_test.csv'),\
+                'scale':False})
 
-class GetData(luigi.WrapperTask):
+
+class SecondWeekLinearMethods(luigi.Task):
     """
-    Получение данных
+    Задача решающая задание по линейным методам классификации
     """
-    @property
-    def result_file_path(self):
-        """
-        Метод получения пути к файлу результатов
-        """
-        return os.environ.get('RESULT_DIR', '.') + '/' +\
-            strip_meta_path(self.meta) + '_gotten_data.csv'
+    param = luigi.parameter.DictParameter(default={
+        'train':path_to_data('./train.csv'),
+        'test':path_to_data('./test.csv'),
+        'scale':False
+    })
+    X_col = [1, 2]
+    y_col = [0]
 
     def output(self):
         """
         Сохраняем данные
         """
-        return luigi.LocalTarget(self.result_file_path)
+        return luigi.LocalTarget(path_to_result(self.__class__.__name__ +\
+            '_{0}_'.format(self.param.get('scale')) +'.txt'))
+
+    @property
+    def X_train_path(self):
+        """
+        СТроит путь к данным для обучения
+        """
+        out = 'SecondWeekLinearMethods_X_train' +\
+                '_{0}_'.format(self.param.get('scale')) + '.csv'
+        return path_to_data(out)
+
+    @property
+    def y_train_path(self):
+        """
+        СТроит путь к целевой переменной данным для обучения
+        """
+        out = 'SecondWeekLinearMethods_y_train' +\
+                '_{0}_'.format(self.param.get('scale')) + '.csv'
+        return path_to_data(out)
+
+
+    @property
+    def X_test_path(self):
+        """
+        СТроит путь к  данным для теста
+        """
+        out = 'SecondWeekLinearMethods_X_test' +\
+                '_{0}_'.format(self.param.get('scale')) + '.csv'
+        return path_to_data(out)
 
     def run(self):
         """
+        Пуск задачи
         """
-        pass
+        X_train = pd.read_csv(self.X_train_path, usecols=self.X_col)
+        y_train = pd.read_csv(self.y_train_path, usecols=self.y_col)
+
+        X_test = pd.read_csv(self.X_test_path)
+
+        if self.param.get('scale'):
+            scaler = StandardScaler()
+            X_train = scaler.fit_transform(X_train)
+            X_test = scaler.transform(X_test)
+
+        perceptron = Perceptron(random_state=241)
+        perceptron.fit(X_train, y_train)
+
+        perceptron.predict(X_test)
+
+        with self.output().open('w') as output:
+            output.write(perceptron.scale)
+
+    def requires(self):
+        """
+        Что требует
+        """
+        return GetData({'target':self.param.get('train'), 'result':self.X_path,\
+                'coluse':self.X_col}),\
+            GetData({'target':self.param.get('train'), 'result':self.y_path,\
+                'coluse':self.y_col})
+
+class GetData(luigi.WrapperTask):
+    """
+    Получение данных
+    """
+    param = luigi.parameter.DictParameter(default={'target':path_to_data('./data.csv'),\
+            'result':path_to_data('gotten_data.csv'), 'coluse':[1]})
+
+    def output(self):
+        """
+        Сохраняем данные
+        """
+        return luigi.LocalTarget(self.param.get('result'))
+
+    def run(self):
+        """
+        Берем данные
+        """
+        dframe = pd.read_csv(self.param.get('target'),\
+            usecols=self.param.get('usecols'))
+
+        dframe.to_csv(self.param.get('result'))
+
 
 if __name__ == "__main__":
     luigi.run(main_task_cls=TasksSet)
